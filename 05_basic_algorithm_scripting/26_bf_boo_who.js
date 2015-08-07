cb570f87ca5653bd9130cde0ad4d2d4623369ba6
Eric Conde <condeeric@live.com>
2015-08-07 16:07:56 +0800

    bonfire-boo-who solution

---
+++ 05_basic_algorithm_scripting/26_bf_boo_who.js
@@ -0,0 +1,8 @@
+// http://www.freecodecamp.com/challenges/bonfire-boo-who
+
+function boo(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  return typeof bool === 'boolean';
+}
+
+boo(null);
