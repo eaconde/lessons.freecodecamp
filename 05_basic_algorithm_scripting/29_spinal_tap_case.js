e11c7c77eb4f901bfe0be4bfc3937a048e467b97
Eric Conde <condeeric@live.com>
2015-08-10 17:38:23 +0800

    bonfire-spinal-tap-case solution

---
+++ 05_basic_algorithm_scripting/29_spinal_tap_case.js
@@ -0,0 +1,18 @@
+// http://www.freecodecamp.com/challenges/bonfire-spinal-tap-case
+
+function spinalCase(str) {
+    // "It's such a fine line between stupid, and clever."
+    // --David St. Hubbins
+    var newStr = str.replace(/([A-Z])/g, function(match, _, index) {
+        var previous = str[index - 1];
+        if (previous.match(/[\s|\_]/g).length > 0 || previous === undefined) {
+            return match;
+        } else {
+            return " ".concat(match); // add space when uppercased
+        }
+    }).toLowerCase().replace(/[\s|_]/g, '-');
+    console.log(newStr);
+    return newStr;
+}
+
+spinalCase('thisIsSpinalTap');
