db12b6dbefff25d6e55ea0b583aa018e3e5cea0f
Eric Conde <condeeric@live.com>
2015-08-11 11:35:15 +0800

    Added functional programming - traversing arrays

---
+++ 08_functional_programming/1_working_with_arrays/01_traversing_print_all_the_names_in_an_array.js
@@ -0,0 +1,12 @@
+// Exercise 1: Print all the names in an array
+
+function(console) {
+	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"],
+		counter;
+
+	for(counter = 0; counter < names.length; counter++) {
+		console.log(names[counter]);
+	}
+}
+
+// Ask yourself this question: did we need to specify the order in which the names were printed? If not, why do it?
---
+++ 08_functional_programming/1_working_with_arrays/02_traversing_use_forEach_to_print_all_the_names_in_an_array.js
@@ -0,0 +1,11 @@
+// Exercise 2: Use forEach to print all the names in an array
+
+function(console) {
+	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
+
+	names.forEach(function(name) {
+		console.log(name);
+	});
+}
+
+// Notice that forEach lets us specify what we want to happen to each item in the array, but hides how the array is traversed.
