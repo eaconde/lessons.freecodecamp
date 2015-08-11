59140ef70b73e4e2fe80e6c8a9ccfe617a42b59d
Eric Conde <condeeric@live.com>
2015-08-11 11:42:06 +0800

    removed function to be executable

--- 08_functional_programming/1_working_with_arrays/exercise_01.js
+++ 08_functional_programming/1_working_with_arrays/exercise_01.js
@@ -1,12 +1,10 @@
 // Exercise 1: Print all the names in an array
 
-function(console) {
-	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"],
-		counter;
+var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"],
+	counter;
 
-	for(counter = 0; counter < names.length; counter++) {
-		console.log(names[counter]);
-	}
+for(counter = 0; counter < names.length; counter++) {
+	console.log(names[counter]);
 }
 
 // Ask yourself this question: did we need to specify the order in which the names were printed? If not, why do it?
--- 08_functional_programming/1_working_with_arrays/exercise_02.js
+++ 08_functional_programming/1_working_with_arrays/exercise_02.js
@@ -1,11 +1,10 @@
 // Exercise 2: Use forEach to print all the names in an array
 
-function(console) {
-	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
+var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
 
-	names.forEach(function(name) {
-		console.log(name);
-	});
-}
+names.forEach(function(name) {
+	console.log(name);
+});
+
 
 // Notice that forEach lets us specify what we want to happen to each item in the array, but hides how the array is traversed.
