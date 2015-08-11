8ca52fb20f2a390848ff2f6a0ae5be2c35744b7d
Eric Conde <condeeric@live.com>
2015-08-12 00:48:36 +0800

    updated comment

--- 06_object_oriented_javascript/6_wp_build_objects_with_prototypal_classes/1_improving_performance.js
+++ 06_object_oriented_javascript/6_wp_build_objects_with_prototypal_classes/1_improving_performance.js
@@ -14,7 +14,7 @@ Car.methods = {
 };
 
 // ======================================
-// using protypes... (pseudo-classical pattern)
+// using protypes... (this will make way for creating a pseudo-classical pattern)
 var Car = function(loc) {
   var obj = Object.create(Car.prototype);
   obj.loc = loc;
