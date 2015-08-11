68ecd57612079e38aece3d392b0eff0c98ba1efa
Eric Conde <condeeric@live.com>
2015-08-11 23:02:58 +0800

    add functional classes

---
+++ 06_object_oriented_javascript/5_wp_build_objects_with_functional_classes/1_decorator_code_vs_classes.js
@@ -0,0 +1,28 @@
+
+// decorator code requires the oj
+// A class builds the object that it is going to augment vs decorator requires the object as its input
+
+/************************************
+** // decorator we used in previous task
+** var carLike = function(obj, loc) {
+**   obj.loc = loc;
+**   obj.move = function() {
+**     obj.loc++;
+**   }
+**   return obj;
+** }
+**
+************************************/
+
+var Car = function(loc) {
+  var obj = {loc: loc};
+  obj.move = function() {
+    obj.loc++;
+  }
+  return obj;
+};
+
+var amy = Car(1);
+amy.move();
+var ben = Car(9);
+ben.move();
