17011cd24b9999589c1af375c83ecdda0bc8a28d
Eric Conde <condeeric@live.com>
2015-08-21 14:24:41 +0800

    clean up index

--- 12_angularjs/01_get_started_with_angularjs/modules/app.js
+++ 12_angularjs/01_get_started_with_angularjs/modules/app.js
@@ -3,7 +3,7 @@
     { name: 'Dodecahedron', price: 2.95, description: '. . .', canPurchase: true, soldOut: false},
     { name: 'Gem2', price: 4.95, description: '. . .', canPurchase: true, soldOut: true},
     { name: 'Pentagonal Gem', price: 3.95, description: '. . .', canPurchase: false, soldOut: false},
-    { name: 'Gem4', price: 5.95, description: '. . .', canPurchase: true, soldOut: false}
+    { name: 'Zircon', price: 5.95, description: '. . .', canPurchase: true, soldOut: false}
   ];
   var app = angular.module('store', [ ]);
 
